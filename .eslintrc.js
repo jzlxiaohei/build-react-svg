13e1e9f50cb1792167b4aa6cfc7e6c0337f7a7ed
zilong <leonard.jiang@liulishuo.com>
2017-10-28 17:51:43 +0800

    feat(storybook): add storybook to view demo, and add corresponding eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,31 @@
 module.exports = {
   extends: [
-    "eslint:recommended"
+    "eslint:recommended",
+    "plugin:react/recommended",
   ],
   parser: "babel-eslint",
   env: {
-    "node": true,
-    "es6": true,
-    "browser": true,
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
   },
+  parserOptions: {
+    ecmaFeatures: {
+        jsx: true
+    },
+  },
+  plugins: [
+    "import",
+    "react",
+    "jsx-a11y"
+  ],
   rules: {
     "arrow-body-style": 0,
     "newline-after-var": 0,
     "no-return-assign": 0,
     "no-underscore-dangle": 0,
     "prefer-template": 0,
+    "react/display-name", 0
   },
 };
